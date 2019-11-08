f16a5ea36504263efe1fb7987b34ab97b56fa05e
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-08 13:09:57 -0500

    SQLite studio data testing complete

--- data/migrations/20191108114826_001_project_tracker.js
+++ data/migrations/20191108114826_001_project_tracker.js
@@ -1,33 +1,23 @@
 
 exports.up = function(knex) {
-    return knex.schema
-    .createTable('project', tbl => {
+    return knex.schema.createTable('project', tbl => {
         tbl.increments()
 
         tbl.string('project_name').notNullable()
         tbl.string('description')
-        tbl.boolean('completed').notNullable().defaultTo(false)
-    })
-
-    .createTable('resource', tbl => {
-        tbl.increments()
-
-        tbl.string('resource_name').notNullable()
-        tbl.string('description')
+        tbl.boolean('completed')
+        .notNullable()
+        .defaultTo(false)
     })
 
     .createTable('task', tbl => {
         tbl.increments()
 
         tbl.string('description').notNullable()
-        tbl.string('notes').notNullable()
-        tbl.boolean('completed').notNullable().defaultTo(false)
+        tbl.string('notes')
+        tbl.boolean('completed').defaultTo(false).notNullable()
 
-        tbl.integer('project_id')
-        .unsigned()
-        .inTable('project')
-        .references('id')
-        .notNullable()
+        tbl.integer('project_id').references('id').inTable('project').unsigned().notNullable()
         .onUpdate('CASCADE')
         .onDelete('CASCADE')
     })
@@ -35,21 +25,21 @@ exports.up = function(knex) {
     .createTable('project_resource', tbl => {
         tbl.increments()
 
-        tbl.integer('project_id')
-        .unsigned()
-        .inTable('project')
-        .references('id')
-        .notNullable()
+        tbl.integer('project_id').references('id').inTable('project').unsigned().notNullable()
         .onUpdate('CASCADE')
         .onDelete('CASCADE')
 
-        tbl.integer('resource_id')
-        .unsigned()
-        .inTable('resource')
-        .references('id')
-        .notNullable()
+        tbl.integer('resource_id').references('id').inTable('resource').unsigned().notNullable()
         .onUpdate('CASCADE')
         .onDelete('CASCADE')
+    })
+
+    .createTable('resource', tbl => {
+        tbl.increments()
+
+        tbl.string('resource_name')
+        .notNullable()
+        tbl.string('description')
     })
 };
 
